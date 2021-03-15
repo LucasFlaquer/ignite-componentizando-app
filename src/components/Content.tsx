b33b822f693fcc71a0889deb205e470314026b50
LucasFlaquer <lucas.flaquer@gmail.com>
2021-03-15 19:58:22 -0300

    componentizacao

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
+import { GenreResponseProps, MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps
+  movies: MovieProps[]
+}
+
+
+export function Content({ selectedGenre, movies }: ContentProps) {
   // Complete aqui
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
